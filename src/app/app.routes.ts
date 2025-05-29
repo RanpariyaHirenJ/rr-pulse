18b86ba0319aadc59f7aaf7e000d372c6c3a1046
RanpariyaHirenJ <hirenatoffice@gmail.com>
2025-05-29 10:24:36 +0530

    Del Proj

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [    
-  { path: 'index', component: HomeComponent },
-  
-  { path: '**', redirectTo: 'index', pathMatch: 'full' }
-];
--- src/assets/js/custom.js
+++
@@ -1,5 +0,0 @@
- window.addEventListener('load', function() {
-    setTimeout(function() {
-        document.getElementById('pre-load').style.display = 'none';
-    }, 300);
-});
